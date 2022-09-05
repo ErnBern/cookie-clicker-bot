7d73ec9f77a5201cb045e2e83d6cdc08105542ae
ErnBern <111699607+ErnBern@users.noreply.github.com>
2022-09-05 09:44:08 -0500

    Update v1.py

--- v1.py
+++ v1.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 
 
-ser = Service('C:\Program Files (x86)\chromedriver.exe')
+ser = Service('Enter Your Chromdriver Path')
 op = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=ser, options=op)
 
